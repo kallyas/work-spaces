d27482cb76c8e1a77a0bb76725bdcb195e8e997a
Kally <kallyasmedia@gmail.com>
2021-09-05 11:38:55 +0000

    add new components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
+import BottomCard from './components/BottomCard';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <BottomCard />
       <Footer />
     </div>
   );
