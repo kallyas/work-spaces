01385866dde14bfd30b951d9d4172fb2ca39f358
Kally <kallyasmedia@gmail.com>
2021-09-12 10:20:48 +0000

    finish homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
+import FooterMapCard from './components/FooterMapCard';
+import Testimonials from './components/Testimonials';
+import HowItWorks from './components/HowItWorks';
+import MainSection from './components/MainSection';
+import Layout from './components/Layout';
+import IntroSection from './components/IntroSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Layout>
+      <IntroSection />
+       <MainSection />
+      <HowItWorks />
+      <Testimonials />
+      <FooterMapCard />
       <Footer />
-    </div>
+      </Layout>
   );
 }
 
